e1ae91fe2ea62cac6110b2cd090b7d17c83aaec2
Pedro Soares <54991781+PedroS235@users.noreply.github.com>
2024-01-29 13:13:50 +0100

    feat: add a pop-up alert when submiting
    Signed-off-by: Pedro Soares <54991781+PedroS235@users.noreply.github.com>

--- js/script.js
+++ js/script.js
@@ -8,4 +8,5 @@ function validateForm() {
   console.log('Message: ' + message);
 
   console.warn('This form is not yet configured to send emails.');
+  alert('This form is not yet configured to send emails. Send an email to pmbs.123@gmail.com if you want to contact me');
 }
